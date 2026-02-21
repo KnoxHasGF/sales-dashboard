2e1125a794389c5c03f4cc86476a480242c68418
KnoxHasGF <166000165+KnoxHasGF@users.noreply.github.com>
2026-02-21 16:16:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/sales-dashboard/',
   plugins: [react()],
-  base: "/sales-dashboard/",   // 👈 MUST match repo name
-});
+})
